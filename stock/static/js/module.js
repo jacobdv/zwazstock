95c9490874b9eda5fdb1b510bdd475d9c96d8df4
Jacob de Vries <mrjacobdevries@gmail.com>
2021-05-14 13:21:14 -0700

    Hmm. Branching to work on flask app instead.

---
+++ stock/static/js/module.js
@@ -0,0 +1,24 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = 'mongodb://localhost:27017/ZwazStocks';
+const DATABASE_NAME = "ZwazStocks";
+
+
+var app = Express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+        
+        collection = database.collection("test");
+    });
+});
