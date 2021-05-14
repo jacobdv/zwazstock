8e5250339c2578194504463405cbf787c5b9ecc2
Jacob de Vries <mrjacobdevries@gmail.com>
2021-05-13 21:39:13 -0700

    Mongo connection established.

--- stock/static/js/createDB.js
+++ stock/static/js/createDB.js
@@ -1,12 +1,11 @@
-let MongoClient = require('mongodb').MongoClient;
-// const url = 'mongodb://localhost:27017/ZwazStocks';
-
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     console.log('Database Created!');
-//     db.close();
-// });
-
-MongoClient.connect('mongodb://localhost:27017/ZwazStock', function(err, db) {
-    let collection = db.collection('Test');
-});
+let MongoClient = require('mongodb').MongoClient;  
+let url = "mongodb://localhost:27017/ZwazStocks";  
+MongoClient.connect(url, function(err, db) {  
+    if (err) throw err;  
+    let dbase = db.db('ZwazStocks');
+    dbase.createCollection("employees", function(err, res) {  
+        if (err) throw err;  
+        console.log("Collection is created!");  
+        db.close();  
+    });  
+});  
