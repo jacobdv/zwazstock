da7ac5bfa8efe9eb4d1532b1cca8f3cca53cba7d
Jacob de Vries <mrjacobdevries@gmail.com>
2021-05-14 14:57:44 -0700

    Working API pull. Need to populate MongoDB.

--- app.py
+++ app.py
@@ -4,70 +4,33 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 from census import Census
 import pymongo
+import requests
+from config import finnhub_API_KEY
+import datetime
 
-# Additional tools for API routes.
-import json
-from bson import json_util
-from bson.json_util import dumps
+# List of stock dictionaries.
+zwazStocks = [{'symbol': 'AAL','shares': 8 },{'symbol': 'AAPL','shares': 2.003},{'symbol': 'AGRO','shares': 6},{'symbol': 'ATVI','shares': 2.01 },{'symbol': 'DIN','shares': 2},{'symbol': 'F','shares': 5},{'symbol': 'HEXO','shares': 8},{'symbol': 'LAZR','shares': 4},{'symbol': 'NKE','shares': 11.092},{'symbol': 'OPEN','shares': 5},{'symbol': 'SELB','shares': 20.32},{'symbol': 'VZ','shares': 0.828505},{'symbol': 'ZYNE','shares': 1},]
 
 # Create an instance of Flask.
 app = Flask(__name__)
 CORS(app)
 
-zwazStocks = [
-    {
-        'symbol': 'AAL',
-        'shares': 8
-    },{
-        'symbol': 'AAPL',
-        'shares': 2.003
-    },{
-        'symbol': 'AGRO',
-        'shares': 6
-    },{
-        'symbol': 'ATVI',
-        'shares': 2.01
-    },{
-        'symbol': 'DIN',
-        'shares': 2
-    },{
-        'symbol': 'F',
-        'shares': 5
-    },{
-        'symbol': 'HEXO',
-        'shares': 8
-    },{
-        'symbol': 'LAZR',
-        'shares': 4
-    },{
-        'symbol': 'NKE',
-        'shares': 11.092
-    },{
-        'symbol': 'OPEN',
-        'shares': 5
-    },{
-        'symbol': 'SELB',
-        'shares': 20.32
-    },{
-        'symbol': 'VZ',
-        'shares': 0.828505
-    },{
-        'symbol': 'ZYNE',
-        'shares': 1
-    },
-]
-
 # Creating the database.
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 zwazStocksDB = client['ZwazStocks']
-# Connections to both collections in MongoDB.
+
+# Connections to the MongoDB database.
 app.config["DEBUG"] = True
 app.config["MONGO_URI"] = "mongodb://localhost:27017/ZwazStocks"
 mongo = PyMongo(app)
+
+# Creating overview collection.
 collection = zwazStocksDB['Share Overview']
-# Clears the collection for the updated info.
+# Clears the collection for inputting the updated info.
 collection.delete_many({})
 
+
+# Adding overview information for each stock.
 for stock in zwazStocks:
     stockItems = stock.items()
     for key, value in stockItems:
@@ -77,25 +40,30 @@ for stock in zwazStocks:
             shares = value
     # Inserts share information for each stock.
     x = collection.insert_one({ 'symbol': symbol, 'shares': shares })
+    
+    # API request for each stock.
+    response = requests.get(f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={finnhub_API_KEY}')
+    if response.status_code == 200:
+        stockDict = response.json()
+        stockDict = {
+            'meta': stock,
+            'prices': {
+                'open': stockDict['o'],
+                'current': stockDict['c'],
+                'high': stockDict['h'],
+                'low': stockDict['l'],
+                'previousClose': stockDict['pc']
+            },
+            'date': (datetime.datetime.fromtimestamp(stockDict['t']).strftime('%Y-%m-%d')),
+        }
+        print(stockDict)
+    else:
+        print(f'There seems to have been an error with {symbol}')
 
 # Home route that displays index.html content.
 @app.route("/")
 def index():
     return render_template("index.html")
-
-# API endpoint for cities in an individual state, defaulting to Oregon.
-# @app.route("/api/cities/", defaults={'state': 'OR'})
-# @app.route("/api/cities/<state>/", methods=['GET', 'POST'])
-# def cityData(state):
-#     stateCitiesList = dumps(citiesCollection.find({'State': state}, {'_id': 0}))
-#     return stateCitiesList
-
-# # API endpoint for states' aggregated data.
-# @app.route("/api/states/", methods=['GET'])
-# def stateData():
-#     # Creates a list from the collection and uses json_util to return this result.
-#     statesList = list(statesCollection.find())
-#     return json.dumps(statesList, default = json_util.default)
 
 # Do the thing. (:
 if __name__ == "__main__":
